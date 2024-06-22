f75ba084f6875016fe9d223d6c77f7eb3b810390
Robert Yohnka <robertyohnka@Roberts-MacBook-Pro.local>
2024-06-22 17:54:46 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+import ListAwardDetails from "./ListAwardDetails";
+import ListAwardxInv from "./ListAwardxInv";
+import ListInvxDept from "./ListInvxDept";
+import ListRASxDeptnoLogin from "./ListRASxDeptnoLogin";
+import './App.css'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
